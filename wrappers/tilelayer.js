864fa845b6af53cfd53a44f5b8b4c19131773d3f
Ingo Schommer <ingo@silverstripe.com>
2015-01-13 19:23:31 +1300

    UNFINISHED Custom tile layers
    Required for esri-leaflet integration.
    Currently stuck at actual requiring of the esri-leaflet module
    without hardwiring it into Mappy. It would need to load
    after leaflet but before Mappy since the Mappy init code
    parses the config and tries to access L.esri already.

--- wrappers/tilelayer.js
+++ wrappers/tilelayer.js
@@ -1,7 +1,23 @@
 'use strict';
 
-var L            = require('../vendor/leaflet.js')
+var L = require('../vendor/leaflet.js')
+var assert = require('assert')
 require('../vendor/leaflet-plugins-google')
+
+/**
+ * Returns the value along a certain path of nested object properties.
+ * 
+ * @param  Object root Base object to traverse (e.g. window)
+ * @param  String path Dot delimited path of object properties
+ * @return Object
+ */
+var objFromPath = function(root, path) {
+	var obj = root, parts = path.split('.');
+	for (var i = 0; i < parts.length; i++) {
+		obj = obj[parts[i]];
+	}
+	return obj;
+}
 
 /**
  * Intermediary tile layer used to generate either a Google layer, or a Leaflet tileLayer.
@@ -12,7 +28,11 @@ require('../vendor/leaflet-plugins-google')
 module.exports = function (name, config) {
   var tileLayer = null
 
-  if(config.type === 'google') {
+  if(config.constructor) {
+  	var args = config.arguments, fn = objFromPath(window, config.constructor);
+  	assert(typeof(fn), 'function', '"constructor" needs to be a function');
+  	tileLayer = fn.call(args);
+  } else if(config.type === 'google') {
     tileLayer = new L.Google((config.mapType ? config.mapType : 'ROADMAP'))
   } else {
     tileLayer = L.tileLayer(config.url, config)
